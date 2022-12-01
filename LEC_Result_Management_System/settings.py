14ac6bff69c5d3abde35894796cefe432f7ee780
unknown <anishshah9876@gmail.com>
2022-12-01 08:49:16 +0545

    CHANGES

--- LEC_Result_Management_System/settings.py
+++ LEC_Result_Management_System/settings.py
@@ -107,23 +107,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR,'static')
 
 ]
+STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles_build', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles_build', 'static')
-
-
 AUTH_USER_MODEL ='LEC_RMSapp.CustomUser'
