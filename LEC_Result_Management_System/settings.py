3159ec11c142e4ff2d8166d21a4ae752ad66afe5
unknown <anishshah9876@gmail.com>
2022-12-01 18:41:20 +0545

    DONE

--- LEC_Result_Management_System/settings.py
+++ LEC_Result_Management_System/settings.py
@@ -114,8 +114,9 @@ import os
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR,'static')
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles_build', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 
 
 # Default primary key field type
