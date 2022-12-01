1895e148778a6bb348343ea480efa9a3d558f0ce
unknown <anishshah9876@gmail.com>
2022-12-01 08:58:34 +0545

    CHANGEMADE

--- LEC_Result_Management_System/settings.py
+++ LEC_Result_Management_System/settings.py
@@ -110,12 +110,13 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
+import os
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR,'static')
-
 ]
-STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles_build', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles_build', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
